8a324f3740d5840b407e746007ec8ac4c224dc0c
RainyCityDiver <cvmembership@posteo.net>
2022-07-07 19:21:56 -0700

    Made headway on 2/3 routes.

--- dataInterface/items.js
+++ dataInterface/items.js
@@ -2,24 +2,35 @@ const uuid = require('uuid');
 
 module.exports = {};
 
-module.exports.items = [{ id: '7', field: 'example' }];
+module.exports.items = [
+  { id: '7', field: 'example' }, 
+  { id: '9', field: 'example two' }];
 
 module.exports.getAll = () => {
   return module.exports.items;
 }
 
 module.exports.getById = (itemId) => {
-  // TODO: complete writing this function
+  // const item = items.find(element => element[itemId] == req.params.id);
+  return module.exports.item[id];
 }
 
 module.exports.deleteById = async (itemId) => {
     // TODO: complete writing this function
 }
 
-module.exports.updateById = async (itemId, newObj) => {
+module.exports.updateById = (itemId, newObj) => {
     // TODO: complete writing this function
+    let indexInItems = module.exports.items.findIndex((item) => item.id == itemId);
+    console.log(indexInItems);
+    if (indexInItems >= 0) {
+      module.exports.items[indexInItems].field = newObj;
+      console.log(module.exports.items[indexInItems])
+    } 
+    return module.exports.items[indexInItems];
+    // console.log(indexInItems);
 }
-
+exampleexample
 module.exports.create = async (item) => {
   const id = uuid.v4();
   const newItem = { ...item, id };
--- routes/items.js
+++ routes/items.js
@@ -7,9 +7,14 @@ router.get("/", (req, res, next) => {
   res.status(200).send(itemData.getAll())
 });
 
+// curl http://localhost:5000/items/id#
 router.get("/:id", (req, res, next) => {
-  // TODO: complete writing this route handler
-  res.status(501).send({ error: 'route not yet implemented' });
+  const answer = itemData.items.find(item => item.id == req.params.id);
+  if (answer) {
+    res.send(answer)
+  } else{
+    res.status(404).send({ error: `That id doesn't exist.` });
+  }
 });
 
 router.post("/", (req, res, next) => {
@@ -19,7 +24,25 @@ router.post("/", (req, res, next) => {
 
 router.put("/:id", (req, res, next) => {
   // TODO: complete writing this route handler
-  res.status(501).send({ error: 'route not yet implemented' });
+  
+  let answerTwo = itemData.updateById(req.params.id, req.body);
+  console.log(answerTwo);
+  res.send(answerTwo);
+  
+  // 1)
+  // let id = +req.params.id;
+  // let indexInItems = itemData.items.findIndex((item) => item.id === id);
+  
+  // 2)
+  // let indexInItems = itemData.items.find((item) => item.id == req.params.id);
+  
+  // let body = req.body;
+  // if (indexInItems >= 0) {
+    // let updatedItem = { id:id, ...body };
+    // itemData[indexInItems] = updatedItem;
+  // } else {
+  // res.status(404).send({ error: `That id doesn't exist` });
+  // }
 });
 
 
