183af0f917b17b570bd77bedd012e425865fa93f
pablomarcel <pablomarcel@outlook.com>
2022-07-10 17:58:21 -0700

    completed routes\items.js

--- routes/items.js
+++ routes/items.js
@@ -9,7 +9,12 @@ router.get("/", (req, res, next) => {
 
 router.get("/:id", (req, res, next) => {
   // TODO: complete writing this route handler
-  res.status(501).send({ error: 'route not yet implemented' });
+  const theItem = itemData.items.find(item => item.id === req.params.id)
+  if (theItem){
+    res.json(theItem)
+  } else{
+    res.status(501).send({ error: 'route not yet implemented' });
+  }
 });
 
 router.post("/", (req, res, next) => {
@@ -19,13 +24,36 @@ router.post("/", (req, res, next) => {
 
 router.put("/:id", (req, res, next) => {
   // TODO: complete writing this route handler
-  res.status(501).send({ error: 'route not yet implemented' });
+
+  const { field } = req.body
+  const theItem = itemData.items.find(item => item.id === req.params.id)
+  console.log(theItem)
+  if (!theItem){
+    res.status(501).send({ error: 'route not yet implemented' });
+  } else{
+    theItem.field=field
+    res.status(200).json(theItem)
+  }
+
 });
 
 
 router.delete("/:id", (req, res, next) => {
   // TODO: complete writing this route handler
-  res.status(501).send({ error: 'route not yet implemented' });
+
+  const theItemIndex = itemData.items.findIndex(item => item.id === req.params.id)
+  console.log(theItemIndex)
+
+  //res.status(501).send({ error: 'route not yet implemented' });
+
+  if (typeof itemData.items[theItemIndex]==='undefined'){
+    res.status(501).send({ error: 'route not yet implemented' });
+  } else{
+    itemData.items.splice(theItemIndex,1)
+    console.log(itemData.items)
+    res.sendStatus(200)
+  }
+
 });
 
 module.exports = router;
