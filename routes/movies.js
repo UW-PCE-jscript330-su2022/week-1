2ab1911973b57bdb671ec90f012dc62d52da27e2
pablomarcel <pablomarcel@outlook.com>
2022-07-12 18:44:23 -0700

    starting working on mongo branch

---
+++ routes/movies.js
@@ -0,0 +1,54 @@
+const { Router } = require("express");
+const router = Router();
+
+const movieData = require('../dataInterface/movies');
+
+router.get("/", (req, res, next) => {
+    res.status(200).send(movieData.getAll())
+});
+
+router.get("/:id", (req, res, next) => {
+
+    const theMovie = movieData.getById(req.params.id)
+    if (theMovie){
+        res.json(theMovie)
+    } else{
+        res.status(404).send({ error: 'not found' });
+    }
+});
+
+router.post("/", (req, res, next) => {
+    movieData.create(req.body);
+    res.sendStatus(200);
+});
+
+router.put("/:id", (req, res, next) => {
+
+    const { id, field } = req.body
+    const updatedMovie = {
+        id: id,
+        field: field
+    }
+    const theMovie = movieData.updateById(req.params.id, updatedMovie)
+    if (!theMovie){
+        res.status(200).send({ error: 'Movie not found but sending a 200 to indicate no action taken' });
+    } else{
+
+        res.status(200).json(theMovie)
+    }
+
+});
+
+router.delete("/:id", async(req, res, next) => {
+
+    const myData = await movieData.deleteById(req.params.id)
+
+    if (myData){
+        res.sendStatus(200)
+    } else{
+        res.status(404).send({ error: 'not found' });
+    }
+
+});
+
+module.exports = router;
