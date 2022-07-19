e3c322990ff36899a820d303038749e82971317b
jl105 <jilinda10@gmail.com>
2022-07-19 10:40:20 -0700

    Yuhong Mongo DB for Week 2 Assignment

---
+++ dataInterface/movies.js
@@ -0,0 +1,52 @@
+module.exports = {}
+const { MongoClient } = require("mongodb");
+const ObjectId = require('mongodb').ObjectId;
+
+const uri = "mongodb+srv://jilinda10:liaNg331*)@cluster0.f4ghe7b.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+module.exports.getAll = async () => {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    const query = {};
+    let movieCursor = await movies.find(query).limit(10).project({title: 1});
+    return movieCursor.toArray();
+}
+
+module.exports.getById = async (movieId) => {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    const query = {_id: ObjectId(movieId)};
+
+    let movie = await movies.findOne(query);
+
+    return movie;
+}
+
+module.exports.deleteById = (movieId) => {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    return [];
+}
+
+module.exports.updateById = (movieId, newObj) => {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+    return [];
+}
+
+module.exports.create = async (newObj) => {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    let result = await movies.insertOne(newObj);
+    console.log(result);
+
+    if (result.acknowledged) {
+        return {newObjectId: result.insertedId, message: `Item created ID: ${result.insertedId}`};
+    } else {
+        return {message: "ERROR"};
+    }
+}
---
+++ routes/movies.js
@@ -0,0 +1,41 @@
+const { Router } = require("express");
+const router = Router();
+
+const movieData = require('../dataInterface/movies');
+
+// curl http://localhost:5000/movies
+router.get("/", async (req, res, next) => {
+  let movieList = await movieData.getAll();
+
+  res.status(200).send(movieList)
+});
+
+// curl http://localhost:5000/movies/573a1394f29313caabcdf639
+router.get("/:id", async (req, res, next) => {
+  const theMovie = await movieData.getById(req.params.id)
+  if(theMovie){
+    res.status(200).send(theMovie)
+  } else {
+    res.status(404).send({ error: `no item found with id ${req.params.id}` });
+  }
+});
+
+// curl -X POST -H "Content-Type: application/json" -d "{ \"title\": \"new title\" }" "http://localhost:5000/movies"
+router.post("/", async (req, res, next) => {
+  let result = await movieData.create(req.body);
+  res.status(200).send(result);
+});
+
+// curl -X PUT -H "Content-Type: application/json" -d "{ \"title\": \"new title\" }" "http://localhost:5000/movies/62ce40898d272992fae9bbb2"
+router.put("/:id", async (req, res, next) => {
+  let updatedList = await movieData.updateById(req.params.id, req.body)
+  res.status(200).send(updatedList)
+});
+
+// curl -X DELETE http://localhost:5000/movies/7
+router.delete("/:id", (req, res, next) => {
+  const updatedList = movieData.deleteById(req.params.id)
+  res.status(200).send({updatedList: updatedList})
+});
+
+module.exports = router;
