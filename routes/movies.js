b206a12b88a777325e9f74ec0d4e3b2f0f4d3c9c
Joann Cahill <cahilljm@aol.com>
2022-07-19 15:17:42 -0700

    Add return code

--- routes/movies.js
+++ routes/movies.js
@@ -3,7 +3,7 @@ const router = Router();
 
 const movieData = require('../dataInterface/movies');
 
-// curl -sS http://localhost:5000/movies
+// curl -sS http://localhost:5001/movies
 router.get("/", async (req, res, next) => {
     let movieList = await movieData.getAll()
     if (movieList!=[]) {
@@ -14,7 +14,7 @@ router.get("/", async (req, res, next) => {
     next()
 });
 
-// curl -sS http://localhost:5000/movies/titles/Titanic
+// curl -sS http://localhost:5001/movies/titles/Titanic
 router.get("/titles/:title", async (req, res, next) => {
     let movieTitle = await movieData.getByTitle(req.params.title)
     if (movieTitle) {
@@ -25,7 +25,7 @@ router.get("/titles/:title", async (req, res, next) => {
     next()
 });
 
-// curl -sS http://localhost:5000/movies/573a13bcf29313caabd57d52
+// curl -sS http://localhost:5001/movies/573a13bcf29313caabd57d52
 router.get("/:id", async (req, res, next) => {
     const theMovie = await movieData.getById(req.params.id)
     if (theMovie) {
@@ -36,7 +36,7 @@ router.get("/:id", async (req, res, next) => {
     next()
 });
 
-// curl -sS -X POST -H "Content-Type: application/json" -d '{"title":"NEW TITLE"}' http://localhost:5000/movies
+// curl -sS -X POST -H "Content-Type: application/json" -d '{"title":"NEW TITLE"}' http://localhost:5001/movies
 router.post("/", async (req, res, next) => {
     let result = await movieData.create(req.body);
     if (result.newObjectId) {
@@ -47,17 +47,27 @@ router.post("/", async (req, res, next) => {
     next()
 });
 
-// curl -sS -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5000/movies/62d6f0f0d63bf47f8bec31c2
+// curl -sS -X PUT -H "Content-Type: application/json" -d '{"plot":"Sharks..."}' http://localhost:5001/movies/62d72a3f65117358ad62d268
+
 router.put("/:id", async (req, res, next) => {
     let updatedList = await movieData.updateById(req.params.id, req.body)
-    res.status(200).send(updatedList)
+    if (updatedList) {
+        res.status(200).send(updatedList.message)
+    } else {
+        res.status(404).send(updatedList.message)
+    }
     next()
   });
   
-  // curl -sS -X DELETE http://localhost:5000/movies/62d6f0f0d63bf47f8bec31c2
+  // curl -sS -X DELETE http://localhost:5001/movies/62d72a3f65117358ad62d268
   router.delete("/:id", async (req, res, next) => {
     const result = await movieData.deleteById(req.params.id)
-    res.status(200).send(result)
+
+    if (result) {
+        res.status(200).send(result.message)
+    } else {
+        res.status(404).send(result.message)
+    }
     next()
   });
 
